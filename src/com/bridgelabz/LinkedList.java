ef5202fa6ac69f81ee076f2a27c9ec37aa25fcf7
pranalikubade <pranalikubade2000@gmail.com>
2023-01-21 15:39:20 +0530

    Created linked list

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,5 +1,27 @@
 package com.bridgelabz;
 
-public class LinkedList {
+public class LinkedList<E> {
+    Node<E> head;
+    Node<E> tail;
+
+    public void push(E key) {
+        Node<E> newNode = new Node(key);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void display() {
+        Node<E> temp = head;
+        while (temp != null) {
+            System.out.print(temp.key + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 
 }
