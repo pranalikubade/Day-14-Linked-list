bd9de989f31cee6b940ca96dea9ef86115bfa14a
pranalikubade <pranalikubade2000@gmail.com>
2023-01-21 16:10:11 +0530

    Added:Append linked list

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,9 +4,12 @@ public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List");
         LinkedList<Integer> node =new LinkedList();
-        node.push(70);
-        node.push(30);
-        node.push(56);
+//        node.push(70);
+//        node.push(30);
+//        node.push(56);
+        node.append(56);
+        node.append(30);
+        node.append(70);
         node.display();
     }
 }
