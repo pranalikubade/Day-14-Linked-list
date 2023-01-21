59e3a1364389352d2009a3338bee898ac28c319c
pranalikubade <pranalikubade2000@gmail.com>
2023-01-21 15:43:20 +0530

    Created linked list

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -3,5 +3,10 @@ package com.bridgelabz;
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List");
+        LinkedList<Integer> node =new LinkedList();
+        node.push(70);
+        node.push(30);
+        node.push(56);
+        node.display();
     }
 }
